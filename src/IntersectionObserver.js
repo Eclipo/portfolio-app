9226b2f5d53a04a5f8690170e3e98e3ca50a1384
edipSarac <edip.sarac@gmail.com>
2022-11-23 00:58:20 +0100

    minor improvements on navbar

--- src/IntersectionObserver.js
+++ src/IntersectionObserver.js
@@ -1,8 +1,7 @@
-import {PlayAnimation} from "./PlayAnimation.js";
+import { PlayAnimation } from "./PlayAnimation.js";
 import { QuickAboutAnimation } from "./components/QuickFacts/QuickAboutAnimation.js";
 
-  setTimeout(function () {
-
+setTimeout(function () {
   //Sections
   const introSection = document.querySelector("#introSectionID");
   const quickFactsSection = document.querySelector("#quickFacts");
@@ -14,12 +13,7 @@ import { QuickAboutAnimation } from "./components/QuickFacts/QuickAboutAnimation
   const portfolioSection = document.querySelector("#portfolioID");
   const contactSection = document.querySelector("#contactID");
 
-
-  const firstWrapper = [
-    introSection,
-    quickFactsSection,
-    aboutMeSection
-  ];
+  const firstWrapper = [introSection, quickFactsSection, aboutMeSection];
 
   const secondWrapper = [
     skillsSection,
@@ -27,9 +21,9 @@ import { QuickAboutAnimation } from "./components/QuickFacts/QuickAboutAnimation
     resumeSection,
     personalitySection,
     portfolioSection,
-    contactSection
+    contactSection,
   ];
-  
+
   //Nav-link
   const navLinkHomeID = document.getElementById("navLinkHomeID");
   const navLinkQuickID = document.getElementById("navLinkQuickFactsID");
@@ -50,134 +44,136 @@ import { QuickAboutAnimation } from "./components/QuickFacts/QuickAboutAnimation
     navLinkResumeID,
     navLinkPersonalityID,
     navLinkPortfolioID,
-    navLinkContactID
+    navLinkContactID,
   ];
 
   const navLinks = document.querySelectorAll(".nav-link");
 
   const animationQuickFactAbout = new QuickAboutAnimation();
-  
-  let oldIndex = -1;
-  function currentNavLink(inputIndex){
-    if(inputIndex !== oldIndex){
-      if(oldIndex === -1){
-      oldIndex=inputIndex;
-      }
 
-      else{
+  let oldIndex = -1;
+  function currentNavLink(inputIndex) {
+    if (inputIndex !== oldIndex) {
+      if (oldIndex === -1) {
+        oldIndex = inputIndex;
+      } else {
         navLinksID[oldIndex].blur();
-        oldIndex=inputIndex;
+        oldIndex = inputIndex;
       }
     }
 
     navLinks.forEach((el) => {
-        el.classList.remove("navFocus");      
+      el.classList.remove("navFocus");
     });
-  
+
     navLinks[inputIndex].classList.add("navFocus");
-  };
-    
+  }
+
   const firstHandleScrollPosY = (entries, observer) => {
     entries.forEach((entry) => {
-    const currentIndex = Array.from(firstWrapper).indexOf(entry.target);
-    
-    if (entry.isIntersecting) {
-    switch(currentIndex) {
-      
-      // Intro Section - Turn ON QuickAboutAnimation  ...
-      case 0:
-        currentNavLink(0);
-      break;
+      const currentIndex = Array.from(firstWrapper).indexOf(entry.target);
 
-      // Quick Facts Section - Turn off Skills
-      case 1:
-        currentNavLink(1);
-        PlayAnimation('SkillAnimation',1);
-        animationQuickFactAbout.playOrStop(true);
-      break;
+      if (entry.isIntersecting) {
+        switch (currentIndex) {
+          // Intro Section - Turn ON QuickAboutAnimation  ...
+          case 0:
+            currentNavLink(0);
+            break;
 
-      //About me Section - Turn Off Personality
-      case 2:
-        currentNavLink(2);
-        animationQuickFactAbout.playOrStop(true);
-      break;
+          // Quick Facts Section - Turn off Skills
+          case 1:
+            currentNavLink(1);
+            PlayAnimation("SkillAnimation", 1);
+            animationQuickFactAbout.playOrStop(true);
+            break;
 
-      default:
-      // code block
+          //About me Section - Turn Off Personality
+          case 2:
+            currentNavLink(2);
+            animationQuickFactAbout.playOrStop(true);
+            break;
+
+          default:
+          // code block
         }
       }
-    })
-  }
+    });
+  };
 
   const secondHandleScrollPosY = (entries, observer) => {
     entries.forEach((entry) => {
       const currentIndex = Array.from(secondWrapper).indexOf(entry.target);
-        if (entry.isIntersecting) {
-          switch(currentIndex) {
-
+      if (entry.isIntersecting) {
+        switch (currentIndex) {
           //Skills Section -  Turn OFF QuickAbout
-            case 0:
-              currentNavLink(3);
-              animationQuickFactAbout.playOrStop(false);
-              PlayAnimation('SkillAnimation',2);
+          case 0:
+            currentNavLink(3);
+            animationQuickFactAbout.playOrStop(false);
+            PlayAnimation("SkillAnimation", 2);
             break;
 
-            //Major experiences - Turn OFF Skills
-            case 1:
-              currentNavLink(4);
-              PlayAnimation('PersonalityAnimation',1);
+          //Major experiences - Turn OFF Skills
+          case 1:
+            currentNavLink(4);
+            PlayAnimation("PersonalityAnimation", 1);
             break;
 
-            //Resume Overview Section - Turn OFF Personality
-            case 2:
-              currentNavLink(5);
-              PlayAnimation('SkillAnimation',1);
-              PlayAnimation('PortfolioAnimation',1);                
+          //Resume Overview Section - Turn OFF Personality
+          case 2:
+            currentNavLink(5);
+            PlayAnimation("SkillAnimation", 1);
+            PlayAnimation("PortfolioAnimation", 1);
             break;
 
-            case 3:
-              currentNavLink(6);
-              PlayAnimation('PersonalityAnimation',2);
+          case 3:
+            currentNavLink(6);
+            PlayAnimation("PersonalityAnimation", 2);
             break;
 
-            //Portfolio - Turn Off Personality 
-            case 4:
-              currentNavLink(7);
-              PlayAnimation('PortfolioAnimation',2);
+          //Portfolio - Turn Off Personality
+          case 4:
+            currentNavLink(7);
+            PlayAnimation("PortfolioAnimation", 2);
             break;
 
-            case 5:
-              currentNavLink(8);
+          case 5:
+            currentNavLink(8);
             break;
 
-            default:
-          }
+          default:
         }
-      })
-    }
+      }
+    });
+  };
 
   const firstOptions = {
     root: null,
-    rootMargin: '0px',
-    threshold: 0.2
-  }
+    rootMargin: "0px",
+    threshold: 0.2,
+  };
 
   const secondOptions = {
     root: null,
-    rootMargin: '0px',
-    threshold: 0.15
-  }
+    rootMargin: "0px",
+    threshold: 0.5,
+  };
 
-  const firstObserver = new IntersectionObserver(firstHandleScrollPosY, firstOptions);
+  const firstObserver = new IntersectionObserver(
+    firstHandleScrollPosY,
+    firstOptions
+  );
   firstObserver.observe(introSection);
   firstObserver.observe(quickFactsSection);
   firstObserver.observe(aboutMeSection);
 
-  const myObserver = new IntersectionObserver(secondHandleScrollPosY, secondOptions);
+  const myObserver = new IntersectionObserver(
+    secondHandleScrollPosY,
+    secondOptions
+  );
   myObserver.observe(skillsSection);
   myObserver.observe(personalitySection);
   myObserver.observe(resumeSection);
   myObserver.observe(experienceSection);
   myObserver.observe(portfolioSection);
   myObserver.observe(contactSection);
-},100);
+}, 100);
