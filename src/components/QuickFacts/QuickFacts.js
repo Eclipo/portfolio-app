09ab5008dbdf090e8183668eff61f7561573476d
Eclipo <edipsarac.work@gmail.com>
2023-06-08 15:34:48 +0200

    helvete vilket jobb

--- src/components/QuickFacts/QuickFacts.js
+++ src/components/QuickFacts/QuickFacts.js
@@ -19,38 +19,42 @@ const QuickFacts = () => {
                 mindful design & solid code
               </h1>
             </div>
-            <div className="row d-inline-block" id="heroBtns">
-              {" "}
-              <Link smooth={true} to={"resumeOverview"} href="#" tabIndex={3}>
-                <button className="skillNav float-start m-0 me-3">
-                  <h4
-                    className="m-auto"
-                    style={{ color: "var(--primary-white)" }}>
-                    RÉSUMÉ
-                  </h4>
-                </button>
-              </Link>
-              <Link smooth={true} to={"contactID"} href="#" tabIndex={6}>
-                <button id="quickContact" className="skillNav float-start m-0">
-                  <h4
-                    className="m-auto"
-                    style={{ color: "var(--primary-white)" }}>
-                    CONTACT
-                  </h4>
-                </button>
-              </Link>
-              <Link smooth={true} to={"skills"} href="#" tabIndex={1}>
-                <button className="scrollIntro float-start ms-3">
-                  <div className="scrollDown"></div>
-                </button>
-              </Link>
-            </div>
-          </div>
 
-          <div className="col m-0 pb-0">
-            <span className="float-start">
-              <EdipGuy></EdipGuy>
-            </span>
+            <div className="row">
+              <div className="col m-0 mb-4 pb-0">
+                <span className="" style={{ display: "grid", height: "17rem" }}>
+                  <EdipGuy></EdipGuy>
+                </span>
+              </div>
+              <div className="col m-auto" id="heroBtns">
+                {" "}
+                <Link smooth={true} to={"resumeOverview"} href="#" tabIndex={3}>
+                  <button className="skillNav float-start m-0 me-2">
+                    <h4
+                      className="m-auto"
+                      style={{ color: "var(--primary-white)" }}>
+                      RÉSUMÉ
+                    </h4>
+                  </button>
+                </Link>
+                <Link smooth={true} to={"contactID"} href="#" tabIndex={6}>
+                  <button
+                    id="quickContact"
+                    className="skillNav float-start m-0">
+                    <h4
+                      className="m-auto"
+                      style={{ color: "var(--primary-white)" }}>
+                      CONTACT
+                    </h4>
+                  </button>
+                </Link>
+                <Link smooth={true} to={"skills"} href="#" tabIndex={1}>
+                  <button className="scrollIntro float-start ms-2">
+                    <div className="scrollDown"></div>
+                  </button>
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       </div>
